bdf8b5a41e32c6f8f45eff0aac74ad9e97a6cf46
Baye Dieng <bayedieng98@gmail.com>
2021-05-28 12:30:13 +0000

    Checked if dataset was loading properly

--- of_dataset.py
+++ of_dataset.py
@@ -1,14 +1,15 @@
 from torch.utils.data import Dataset
 import numpy as np 
 import torch
+from torch.utils.data.dataloader import DataLoader
 from torchvision.datasets import ImageFolder
+from torchvision.transforms.transforms import ToPILImage, ToTensor
 
 
 class OFDataset(Dataset):
     def __init__(self, root, transform, train=True):
         self.data = ImageFolder(root, transform=transform)
         self.train = train
-    
     def __getitem__(self, idx):
         if self.train:
             images, _ = self.data[idx]
@@ -20,3 +21,8 @@ class OFDataset(Dataset):
         return len(self.data)
 
 
+if __name__ == "__main__":
+    trainset = OFDataset(root="data/of-train-images-color", transform=ToTensor())
+    loader = DataLoader(trainset, batch_size=3, shuffle=False)
+    x,y = iter(loader).next()
+    print(y)
